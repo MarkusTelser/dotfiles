9cfdbeb51dedeb33ea88386b16045cb0bc1530c9
Markus Telser <markus.telser99@gmail.com>
2022-09-14 00:15:41 +0200

    add tmux setup && fzf-tmux-script

---
+++ tmux/tmux-layout.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# workplace-setup (pane-layout & auto-tools-startup),
+
+tmux rename-window -t 0 default
+
+W=$(tmux display -p -t 0 '#{window_width}')
+OFFW=$((W / 10))
+
+tmux send-keys 'clear' Enter
+tmux send-keys 'git status' Enter
+
+tmux split-window -h
+tmux selectp -t 1 
+tmux resize-pane -L $OFFW
+tmux send-keys 'clear && (ls TODO.md &> /dev/null && nvim TODO.md) || nvim' Enter
+
+tmux selectp -t 0
+tmux split-window -v
+tmux selectp -t 1
+tmux send-keys 'clear' Enter
+tmux send-keys 'ls -l' Enter
+
+tmux selectp -t 0
