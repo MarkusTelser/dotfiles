d09ec5f7843aef9399de7bcfe3012e9563236a28
Markus Telser <markus.telser99@gmail.com>
2022-11-21 21:07:25 +0100

    update script, check if current wspace is empty

--- scripts/ewspace.sh
+++ scripts/ewspace.sh
@@ -4,6 +4,13 @@
 # - switches to first empty workspace
 # - in range between 0 and $MAX_DESKTOPS
 
+CURRENT_WORKSPACE=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
+NUMBER_WINDOWS=$(i3-msg -t get_tree | jq -r "recurse(.nodes[]) | select(.name == \"$CURRENT_WORKSPACE\") | recurse(.nodes[]) | select(.window) | .name" )
+# only switch to empty workspace, if current one is filled with atleast one window
+if $NUMBER_WINDOWS ; then
+	exit 0;
+fi
+
 MAX_DESKTOPS=10
 WORKSPACES=$(seq -s '\n' 1 1 ${MAX_DESKTOPS}) # generate list of workspace-numbers
 EMPTY_WORKSPACE=$((i3-msg -t get_workspaces | tr ',' '\n' | grep num | awk -F:  '{print int($2)}' ; \
