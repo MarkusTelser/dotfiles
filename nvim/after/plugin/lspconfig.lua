bdaa8375967d79977a74bb5ef6055ce285c83e4b
Markus Telser <markus.telser99@gmail.com>
2023-02-08 15:55:36 +0100

    fix cmp_nvim_lsp.update_capabilities is deprecated

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -74,9 +74,7 @@ for type, icon in pairs(signs) do
 end
 
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
